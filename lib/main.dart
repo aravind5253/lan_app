eeef8e09c715f05bd1dddc43102cee488af22512
boysi <boysinrhythm@gmail.com>
2020-09-08 12:42:43 +0530

    Noob App

--- lib/main.dart
+++ lib/main.dart
@@ -102,10 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headline4,
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
@@ -117,5 +114,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-  // edgdgdgdgrrgee3hehr
 }
